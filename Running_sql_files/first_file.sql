aa84ccb2a3d1629adfd0f25167ba2d28d6a50c01
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-07-14 23:28:58 +0530

    first_file.sql

---
+++ Running_sql_files/first_file.sql
@@ -0,0 +1,10 @@
+-- C:\Users\hp\Desktop\mysql\mysql\Running_sql_files\Running_sql_files.sql
+
+
+
+CREATE TABLE cats (
+    cat_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100),
+    age INT,
+    PRIMARY KEY (cat_id)
+);
