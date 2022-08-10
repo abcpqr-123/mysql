337525cea79f90612b151d481bd5829218938486
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-08-10 10:35:56 +0530

    14_ig_clone.sql

---
+++ 14_Instagram_clone/14_ig_clone.sql
@@ -0,0 +1,97 @@
+DROP DATABASE ig_clone;
+CREATE DATABASE ig_clone;
+
+USE ig_clone;
+
+
+
+CREATE TABLE users(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+
+
+ CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id)
+);
+
+
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id),
+    PRIMARY KEY (user_id, photo_id)   
+);
+
+
+CREATE TABLE follows (
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+
+CREATE TABLE tags(
+    id INTEGER AUTO_ICREMENT PRIMARY KEY,
+    tag_name VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE 
+
+INSERT INTO users (username) VALUES
+ ('Bluethecat'),
+('CharlieBrown'),
+('ColtSteele');
+
+
+INSERT INTO photos(image_url, user_id) VALUES
+('/alskjd76', 1),
+('/lkajsd98', 2),
+('/90jddlkj', 2);
+
+
+INSERT INTO comments(comment_text, user_id, photo_id) VALUES
+('Meow!', 1, 2),
+('Amezing shoot!', 3, 2),
+('I <3 This', 2, 1);
+
+
+
+INSERT INTO likes(user_id, photo_id) VALUES
+(1,1),
+(2,1),
+(1,2),
+(1,3),
+(3,3);
+
+
+INSERT INTO follows (follower_id, followee_id) VALUES
+(1,2),
+(1,3),
+(3,1),
+(2,3);
+
+
+INSERT INTO follows (follower_id, followee_id)  VALUES (2,1);
